81079a41d2a8a4a2e43ffc28bf043c06283b55a7
Arvens <lklkk2002@naver.com>
2020-07-16 18:10:41 +0900

    Initial commit

---
+++ ApiImageDownloader/app/src/main/java/com/todaysquare/apiimagedownloader/utils/Constants.kt
@@ -0,0 +1,23 @@
+package com.todaysquare.apiimagedownloader.utils
+
+object Constants {
+    class Url {
+        companion object {
+            // URL
+            const val BASE_URL = "https://ddragon.leagueoflegends.com/cdn/"
+
+            //  GET
+            const val GET_CHAMP = "{version}/data/{language}/champion/{id}.json"
+            const val GET_IMAGE = "img/champion/splash/{id}_{num}.jpg"
+
+            //  POST
+
+        }
+    }
+
+    class Param {
+        companion object {
+
+        }
+    }
+}
